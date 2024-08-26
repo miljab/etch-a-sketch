fa3432f177bb8e56dca81effade5bf6277cb6694
Milosz <jablonskimilosz1337@gmail.com>
2024-08-26 18:40:18 +0200

    Add RESET button and minor changes to css
    When user click RESET button grid is cleared and user is asked for the size of new grid.
    Now color of squares after hovering is randomized.

--- script.js
+++ script.js
@@ -2,17 +2,34 @@ const container = document.querySelector(".container");
 
 const CONTAINER_SIZE = 860;
 let size = 16;
+createGrid()
 
-for(let i = 0; i < size * size; i++) {
-    let square = document.createElement('div');
-    square.className = "square";
-    square.style.width = (860/size).toString() + "px";
-    square.style.height = (860/size).toString() + "px";
-    container.appendChild(square);
+function createGrid() {
+    for(let i = 0; i < size * size; i++) {
+        let square = document.createElement('div');
+        square.className = "square";
+        square.style.width = (CONTAINER_SIZE/size).toString() + "px";
+        square.style.height = (CONTAINER_SIZE/size).toString() + "px";
+        container.appendChild(square);
+    }
 }
 
+
 container.addEventListener("mouseover" , (event) => {
-    event.target.style.backgroundColor = "green";
+    let r = Math.floor(Math.random() * 256).toString();
+    let g = Math.floor(Math.random() * 256).toString();
+    let b = Math.floor(Math.random() * 256).toString();
+    let rgb = "rgb(" + r + " " + g + " " + b + ")"
+
+    
+    event.target.style.backgroundColor = rgb;
 })
 
+const button = document.querySelector(".button");
 
+button.addEventListener("click", () => {
+    size = prompt("Enter the number of squares per side for new grid:");
+    if(size > 100) size = 100;
+    container.textContent = '';
+    createGrid();
+})
